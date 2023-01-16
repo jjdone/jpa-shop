4e5846b8fb4c40ffd0be6ad717d408bf67a6649e
jjdone <ehdlfaos@gmail.com>
2023-01-16 13:53:24 +0900

    엔티티 설계

--- src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -8,6 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
+import org.thymeleaf.engine.AttributeDefinition;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.Assert.*;
